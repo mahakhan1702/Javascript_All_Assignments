01f5ad14c0ffe881f6f43aa141bef5c5f0e2a07f
mahakhan1702 <maha.khan0217@gmai.com>
2023-07-21 23:45:58 +0500

    update

---
+++ chapter35-52/app.js
@@ -0,0 +1,99 @@
+//Q1
+// function CurrentDateTime() {
+//   var currentDate = new Date();
+//   var dateTimeString = "Current Date & Time: " + currentDate;
+ 
+// }
+// CurrentDateTime();
+
+
+//Q2
+// function greetUser(firstName, lastName) {
+//   var fullName = firstName + " " + lastName;
+//   var greeting = "Hello, " + fullName ;
+//   console.log(greeting);
+// }
+// greetUser("John","Doe");
+
+
+//Q3
+
+// function add(){
+//     var num1=+prompt("enter number 1");
+//     var num2=+prompt("Enter number 2")
+//     var sum=num1+num2;
+//     return sum;
+// }
+//  var result=add();
+// alert(result);
+
+//Q4
+// function Operation(num1, num2, oper) {
+//   var result;
+
+//   switch (oper) {
+//     case "+":
+//       result = num1 + num2;
+//       break;
+//     case "-":
+//       result = num1 - num2;
+//       break;
+//     case "*":
+//       result = num1 * num2;
+//       break;
+//     case "/":
+//       result = num1 / num2;
+//       break;
+//     default:
+//       return "Invalid operator";
+//   }
+
+
+// }
+
+// var number1 = parseFloat(prompt("Enter the first number:"));
+// var number2 = parseFloat(prompt("Enter the second number:"));
+// var oper = prompt("Enter the operator (+, -, *, /):");
+
+// var outputofoperater = performOperation(number1, number2, oper);
+// console.log(outputofoperater);
+
+
+
+
+//Q5
+// function square(number) {
+//   var result = number * number;
+//   return result;
+// }
+
+
+// var input = parseFloat(prompt("Enter a number:"));
+// var squared = square(input);
+// console.log("The square of " + input + " is: " + squared);
+
+//Q6
+// function computeFactorial(number) {
+//   if (number === 0 || number === 1) {
+//     return 1;
+//   } else {
+//     return number * computeFactorial(number - 1);
+//   }
+// }
+
+// // Example usage
+// var input = parseInt(prompt("Enter a number:"));
+// var factorial = computeFactorial(input);
+// console.log("The factorial of " + input + " is: " + factorial);
+
+
+
+
+
+  
+
+
+
+
+
+
